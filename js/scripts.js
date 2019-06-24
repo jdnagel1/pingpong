2f477e7eded016ad96a4bc9959a918eb8a4f4acc
Jake Nagel <jdnagel1@gmail.com>
2019-06-24 11:28:43 -0700

    add most up to date push

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
+
+
 // user interface logic vvv
 
 $(document).ready(function() {
   $('#pingForm').submit(function(event){
       event.preventDefault();
       var intNum = $('input#number').val();
-      stringNum = [];
+      // stringNum = [];
       pushNum(intNum);
-      checkNum();
+      divNum();
       $("ul#answer").empty();
       answer();
       console.log(stringNum);
@@ -16,48 +18,30 @@ $(document).ready(function() {
   // Business Logic vvv
 
   var stringNum = [];
-  function pushNum(num){
+  function pushNum(num) {
     for(var i = 0; i <= num; i++){
       stringNum.push("" + i);
     };
   };
 
-  // var stringNum = [];
-  // function pushNum(num){
-  //   for(var i = 0; i <= num; i++){
-  //     push("" + i);
-  //   };
-  // };
 
-  function checkNum(){
-  var holdNum = "";
-  for(var x = 0; x < stringNum.length; x++) {
-    holdNum = stringNum[x];
-    if(holdNum.includes("3")){
-      stringNum[x] = "ping";
-    }else if(holdNum.includes("5")){
-      stringNum[x] = "pong!";
-    }else if(holdNum.includes("15")){
-      stringNum[x] = "pingpong!";
+function divNum() {
+var holdNum = "";
+for(var x = 0; x < stringNum.length; x++) {
+  holdNum = stringNum[x];
+  if(holdNum % 3 === 0){
+    stringNum[x] = "ping";
+  }else if(holdNum % 5 === 0){
+    stringNum[x] = "pong";
+  // }else if(holdNum % 15 === 0){
+  //   stringNum[x] = "pingpong!";
     };
   };
 };
 
-// function checkNum(){
-// var holdNum = "";
-// for(var x = 0; x < stringNum.length; x++) {
-//   holdNum = stringNum[x];
-//   if(year % 3 === 0){
-//     stringNum[x] = "ping";
-//   }else if(year % 5 === 0){
-//     stringNum[x] = "pong!";
-//   }else if(year % 15 === 0){
-//     stringNum[x] = "pingpong!";
-//     };
-//   };
-// };
 
-function answer(){
+
+function answer() {
   for(var y = 0; y < stringNum.length; y++){
     $('ul#answer').append("<li>" + stringNum[y] + "</li>");
   };
